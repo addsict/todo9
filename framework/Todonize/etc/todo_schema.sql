8f982b2deb116b728f4149ca0f1aa6ded53fba2f
Yuuki Furuyama <addsict@gmail.com>
2013-09-30 15:54:02 +0900

    add install script

--- framework/Todonize/etc/todo_schema.sql
+++ framework/Todonize/etc/todo_schema.sql
@@ -1,5 +1,8 @@
 create table todo (
     id integer primary key auto_increment,
     title text not null,
-    is_done integer default 0
+    is_done integer default 0,
+    is_doing integer default 0,
+    updated_date timestamp,
+    created_date datetime not null
 );
