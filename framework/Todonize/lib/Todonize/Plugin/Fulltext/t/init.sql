129d2009fdd0793aebb54c0fef8cc86d222c7829
Sho Nakatani <lay.sakura@gmail.com>
2013-09-30 10:25:21 +0900

    bugfix on sql Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- framework/Todonize/lib/Todonize/Plugin/Fulltext/t/init.sql
+++ framework/Todonize/lib/Todonize/Plugin/Fulltext/t/init.sql
@@ -1,8 +1,8 @@
-drop table if exists todo;
+drop table if exists todo_test;
 
-CREATE TABLE todo (
+CREATE TABLE todo_test (
   id integer primary key auto_increment,
   title TEXT, FULLTEXT(title) WITH PARSER mecab
 ) ENGINE=myisam DEFAULT CHARSET=utf8;
 
-insert into todo (title) values ("釣り"), ("aとデート"), ("bとデート"), ("cとデート"), ("デートプラン立てる"), ("池袋のデートプラン立てる");
+insert into todo_test (title) values ("釣り"), ("aとデート"), ("bとデート"), ("cとデート"), ("デートプラン立てる"), ("池袋のデートプラン立てる");
